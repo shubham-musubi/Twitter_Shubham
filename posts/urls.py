07709d051f9c6313bd4491a980edc0b370988a14
styagi9817 <styagi9817@gmail.com>
2021-06-11 02:47:44 +0530

    modified urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<int:post_id>/', views.delete, name='delete'),
-    #path('edit/<int:post_id>/', views.edit, name='edit'),
     path('update/<int:pk>/', views.update_tweet, name='update_tweet'),
     path('postLikeAdd/<int:post_id>', views.postLikeAdd, name='likeAdd'),
 ]
